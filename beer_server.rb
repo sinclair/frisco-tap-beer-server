0d332a61f8d4289f2464acb81a67b208b47b9f79
Sinclair Bain <sinclair.bain@gmail.com>
2011-01-10 12:22:37 -0500

    refactored to extract method to same level of abstraction

--- beer_server.rb
+++ beer_server.rb
@@ -4,32 +4,50 @@ require 'open-uri'
 require 'hpricot'
 require 'json'
 
-get '/beers.json' do 
-  status      =   200
-  headers     =   {'Content-Type'=>'application/json'}
-  beer_list   =   []
-  body        =   {'beers'=>beer_list}
-  key         =   'name'
+helpers do
 
-  url        = 'http://friscogrille.com/beer.php'
-  response   = ''
-  open(url, "User-Agent" => "Ruby/#{RUBY_VERSION}",
-    "From"    => "sinclair.bain@gmail.com",
-    "Referer" => "http://www.friscogrille.com/") { |f|
-    response = f.read
-  }
-
-  collector = []
-  doc   =  Hpricot(response)
-  (doc/'html/body/div#content/div#beer-list/div#keg-beers/ul/li').each do |e|
-    collector << e.inner_html()
-  end
-  (doc/'html/body/div#content/div#beer-list/div#keg-beers/div/ul/li').each do |e|
-    collector << e.inner_html()
+  def scrape_frisco_site()
+    url        = 'http://friscogrille.com/beer.php'
+    response   = ''
+    open(url, "User-Agent" => "Ruby/#{RUBY_VERSION}",
+      "From"    => "sinclair.bain@gmail.com",
+      "Referer" => "http://www.friscogrille.com/") { |f| response = f.read }
+    response
   end
 
-  collector.uniq.sort {|a, b| a.downcase <=> b.downcase}.each {|e| beer_list << {key=>e.strip()} }
+  def build_beer_list(site_html)
+    beer_collector  =   []
+    doc             =   Hpricot(site_html)
+    
+    (doc/'html/body/div#content/div#beer-list/div#keg-beers/ul/li').each do |e|
+      beer_collector << e.inner_html()
+    end
+    (doc/'html/body/div#content/div#beer-list/div#keg-beers/div/ul/li').each do |e|
+      beer_collector << e.inner_html()
+    end
+
+    format_beer_list(beer_collector)
+  end
+
+  def format_beer_list(enumerable)
+    key             =   'name'
+    enumerable.
+        uniq.
+        sort {|a, b| a.downcase <=> b.downcase}.collect {|e| {key=>e.strip()} }
+  end
   
-  [status, headers, "#{params['callback']}(#{body.to_json()});"]
+  def build_response(beer_list)
+    status      =   200
+    headers     =   {'Content-Type'=>'application/json'}
+    body        =   {'beers'=>beer_list}
 
+    [status, headers, "#{params['callback']}(#{body.to_json()});"]
+  end
+  
+end
+
+get '/beers.json' do 
+  response    = scrape_frisco_site()
+  beer_list   = build_beer_list(response)
+  build_response(beer_list)
 end
